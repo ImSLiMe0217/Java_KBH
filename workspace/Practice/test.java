daab724c9fb2d387a8a1ac403557294962304b50
Byeongho Kim <qudgh0217@naver.com>
2026-06-09 09:19:00 +0900

    어제 안올린거 올리고 시작

--- workspace/Practice/test.java
+++ workspace/Practice/test.java
@@ -1,11 +1,14 @@
 package Practice;
 
-import java.time.LocalTime;
-
 public class test {
     void main() {
-        LocalTime now = LocalTime.now();
+        String s1 = "Hello";
+        System.out.println(s1 == "Hello");
 
-        System.out.println(now.getHour());
+        String s2 = new String("Hello");
+        System.out.println(s2 == "Hello");
+
+        String s3 = "Hello";
+        System.out.println(s1 == s3);
     }
 }
