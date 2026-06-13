b417c8017a991e922e76cd93f56b4c9f09561eb3
Byeongho Kim <qudgh0217@naver.com>
2026-06-13 23:19:54 +0900

    개인공부

--- practice/test.java
+++ practice/test.java
@@ -1,15 +1,18 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class test {
     static void main() {
-        ArrayList<Integer> list = new ArrayList<>();
-        int n = 10;
-        while (n != 1) {
-            n = n % 2 == 0 ? n / 2 : 3 * n + 1;
-            list.add(n);
-        }
-//        list.stream().mapToInt(Integer::intValue);
-        System.out.println(list.getLast());
-        list.size();
+        boolean t = true;
+        boolean f = false;
+
+        int a = 1, b = 2, c = 3, d = 0;
+        int[] arr = {a, b, c, d};
+        Arrays.sort(arr);
+
+        ArrayList<Integer> idxList = new ArrayList<Integer>();
+        idxList.stream().count();
+        idxList.isEmpty();
+        
     }
 }
