7e6c8d0e49777a4a241d38eb959ecf686bc37a88
Byeongho Kim <qudgh0217@naver.com>
2026-06-21 20:04:39 +0900

    md파일 최신화

--- Readme.md
+++ Readme.md
@@ -9,25 +9,34 @@ ___
         - IntelliJ 설치
         - JDK 설치
     - ### JAVA 기초
-        - 내용 정리 중
-        - (임시) 날짜별 속기록: [ClassSummaryMD](ClassSummaryMD)
+        - [01. 변수와 상수.md](ClassSummaryMDbyIndex/01.%20Basic/01.%20%EB%B3%80%EC%88%98%EC%99%80%20%EC%83%81%EC%88%98.md)
+        - [02. 연산자.md](ClassSummaryMDbyIndex/01.%20Basic/02.%20%EC%97%B0%EC%82%B0%EC%9E%90.md)
+        - [03. 형변환.md](ClassSummaryMDbyIndex/01.%20Basic/03.%20%ED%98%95%EB%B3%80%ED%99%98.md)
+        - [04. 조건문1.md](ClassSummaryMDbyIndex/01.%20Basic/04.%20%EC%A1%B0%EA%B1%B4%EB%AC%B81.md)
+        - [05. 조건문2.md](ClassSummaryMDbyIndex/01.%20Basic/05.%20%EC%A1%B0%EA%B1%B4%EB%AC%B82.md)
+        - [06. 반복문.md](ClassSummaryMDbyIndex/01.%20Basic/06.%20%EB%B0%98%EB%B3%B5%EB%AC%B8.md)
+        - [07. 메모리.md](ClassSummaryMDbyIndex/01.%20Basic/07.%20%EB%A9%94%EB%AA%A8%EB%A6%AC.md)
+        - [08. 배열.md](ClassSummaryMDbyIndex/01.%20Basic/08.%20%EB%B0%B0%EC%97%B4.md)
+        - [09. 메서드.md](ClassSummaryMDbyIndex/01.%20Basic/09.%20%EB%A9%94%EC%84%9C%EB%93%9C.md)
     - ### 기초 테스트
-- ## 06/01 ~ 06/05
+- ## 06/01 ~ 06/10
     - ### 객체지향 프로그래밍(OOP) 기초
-        - 내용 정리 예정
+        - [10. 객체지향 프로그래밍(기초1).md](ClassSummaryMDbyIndex/02.%20OOP/10.%20%EA%B0%9D%EC%B2%B4%EC%A7%80%ED%96%A5%20%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D%28%EA%B8%B0%EC%B4%881%29.md)
+        - [11. 객체지향 프로그래밍(기초2).md](ClassSummaryMDbyIndex/02.%20OOP/11.%20%EA%B0%9D%EC%B2%B4%EC%A7%80%ED%96%A5%20%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D%28%EA%B8%B0%EC%B4%882%29.md)
     - ### 객체지향 프로그래밍 심화 - 상속
         - 내용 정리 예정
-- ## 06/08 ~ 06/10
     - ### 객체지향 프로그래밍 심화 - 다형성
         - 내용 정리 예정
     - ### 자바 주요 패키지와 API
         - 내용 정리 예정
-- ## 06/11 ~ 06/17
+- ## 06/11 ~ 06/19
     - ### MySQL 기초
     - ### MySQL 심화
     - ### 각종 예제
-- ## 06/17 ~
     - #### JDBC
+- ## 06/19 ~
+    - ### JAVA 심화
+      -
 
 ___
   
--- workspace/04_advanced/ch02/ArrayListTest.java
+++ workspace/04_advanced/ch02/ArrayListTest.java
@@ -1,5 +1,74 @@
 package ch02;
 
-public class ArrayListTest {
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
+public class ArrayListTest {
+    void main() {
+//        String[] fruits = {"바나나", "사과", "오렌지", "파인애들", "수박"};
+//        ArrayList<String> fruits = new ArrayList<>();
+//        LinkedList<String> fruits = new LinkedList<>();
+//        Vector<String> fruits = new Vector<>();
+
+//        HashSet<String> fruits = new HashSet<>();
+        TreeSet<String> fruits = new TreeSet<>();
+//        LinkedHashSet<String> fruits = new LinkedHashSet<>();
+
+        fruits.add("바나나");
+        fruits.add("사과");
+        fruits.add("오렌지");
+        fruits.add("파인애플");
+        fruits.add("수박");
+        fruits.add("사과");
+
+        // List계열과 Set계열의 출력문이 서로 다름
+        // List 계열은 중복데이터 허용, Set계열은 중복 불가능
+        printFruits(fruits);
+        System.out.println(fruits.toString());
+
+    }
+
+    // 전달받은 과일 목록 출력
+    // 최상위
+    void printFruits(Collection<String> fruits) {
+        for (String fruit : fruits) {System.out.print(fruit + " ");}
+        System.out.println();
+    }
+
+    // List류 상위
+    void printFruits(List<String> fruits) {
+        for (String fruit : fruits) {System.out.print(fruit + " ");}
+        System.out.println();
+    }
+
+    //    void printFruits(ArrayList<String> fruits) {
+//        for (String fruit : fruits) {System.out.print(fruit + " ");}
+//        System.out.println();
+//    }
+//
+//    void printFruits(LinkedList<String> fruits) {
+//        for (String fruit : fruits) {System.out.print(fruit + " ");}
+//        System.out.println();
+//    }
+//
+//    void printFruits(Vector<String> fruits) {
+//        for (String fruit : fruits) {System.out.print(fruit + " ");}
+//        System.out.println();
+//    }
+    // Set류 상위
+    void printFruits(Set<String> fruits) {
+        for (String fruit : fruits) {System.out.print(fruit + " ");}
+        System.out.println();
+    }
+//    void printFruits(HashSet<String> fruits) {
+//        for (String fruit : fruits) {System.out.print(fruit + " ");}
+//        System.out.println();
+//    }
+//
+//    void printFruits(TreeSet<String> fruits) {
+//        for (String fruit : fruits) {System.out.print(fruit + " ");}
+//        System.out.println();
+//    }
 }
